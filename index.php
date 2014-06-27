c1984b2f634fd468610ddf667eae3ff10791c3bc
Jamie Warburton <jamiewarburton@live.co.uk>
2014-06-27 12:37:46 +0100

    Updated index.php to include new API page links

--- index.php
+++ index.php
@@ -13,11 +13,17 @@ $title = "";
 					<ul>
 						<li><a href="ec.php">Express Checkout</a></li>
 						<li><a href="lipp.php">LIPP</a></li>
-						<li><a href="lipp.php">Web Payments Pro</a></li>
-						<li><a href="lipp.php">In-Context Checkout</a></li>
+						<li><a href="icc.php">In-Context Checkout</a></li>
 					</ul>
 				</div></li>
-				<li><a class="container" href="#"><span class="add">+</span></a></li>
+				<li><div class="container">
+					<header><span class="head count left">4</span><a href="/mapo/api.php">APIs</a></header>
+					<ul>
+						<li><a href="api-setec.php">SetExpressCheckout</a></li>
+						<li><a href="api-getec.php">GetExpressCheckoutDetails</a></li>
+						<li><a href="api-doec.php">DoExpressCheckoutPayment</a></li>
+					</ul>
+				</div></li>
 			</ul>
 		</div>
 	</section>
