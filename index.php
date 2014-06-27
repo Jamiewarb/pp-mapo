ba6f92f58bc7c1be035a91db95ed7d0cc5c15a52
Jamie Warburton <jamiewarburton@live.co.uk>
2014-06-27 15:21:08 +0100

    Added headline font, started on API page

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $title = "";
 		<div class="one-cont">
 			<ul class="boxes grey">
 				<li><div class="container">
-					<header><span class="head count left">4</span><a href="/mapo/ec.php">Products</a></header>
+					<header><span class="head count left">3</span><a href="/mapo/ec.php">Products</a></header>
 					<ul>
 						<li><a href="ec.php">Express Checkout</a></li>
 						<li><a href="lipp.php">LIPP</a></li>
@@ -17,7 +17,7 @@ $title = "";
 					</ul>
 				</div></li>
 				<li><div class="container">
-					<header><span class="head count left">4</span><a href="/mapo/api.php">APIs</a></header>
+					<header><span class="head count left">3</span><a href="/mapo/api.php">APIs</a></header>
 					<ul>
 						<li><a href="api-setec.php">SetExpressCheckout</a></li>
 						<li><a href="api-getec.php">GetExpressCheckoutDetails</a></li>
--- resources/templates/template-boxes.php
+++ resources/templates/template-boxes.php
@@ -1,1 +1,31 @@
-TEMPLATE
+<?php
+include_once "resources/includes/functions.php";
+$title = "";
+?>
+
+<?php include "$sitepath/resources/includes/header.php"; ?>
+
+	<section>
+		<div class="one-cont">
+			<ul class="boxes grey">
+				<li><div class="container">
+					<header><span class="head count left">3</span><a href="#">Title</a></header>
+					<ul>
+						<li><a href="#">List</a></li>
+						<li><a href="#">List</a></li>
+						<li><a href="#">List</a></li>
+					</ul>
+				</div></li>
+				<li><div class="container">
+					<header><span class="head count left">1</span><a href="#">Title 2</a></header>
+					<ul>
+						<li><a href="#">List</a></li>
+						<li><a href="#">List</a></li>
+						<li><a href="#">List</a></li>
+					</ul>
+				</div></li>
+			</ul>
+		</div>
+	</section>
+
+<?php include "$sitepath/resources/includes/footer.php"; ?>
